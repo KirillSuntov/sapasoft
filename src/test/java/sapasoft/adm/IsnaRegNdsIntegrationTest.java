88605cd28b73dcbdd69a18cc7a067f6ac85e79a7
kirill.suntsov <kirillzuzu@gmail.com>
2022-02-17 02:58:08 +0600

    screenshot

--- src/test/java/sapasoft/adm/IsnaRegNdsIntegrationTest.java
+++ src/test/java/sapasoft/adm/IsnaRegNdsIntegrationTest.java
@@ -9,6 +9,8 @@ import org.junit.runners.MethodSorters;
 import sapasoft.adm.pages.Adm;
 import sapasoft.adm.testconfigs.BaseSetings;
 
+import java.io.IOException;
+
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @DisplayName("НЗ о регистрационном учете по НДС")
 
@@ -16,7 +18,7 @@ public class IsnaRegNdsIntegrationTest extends BaseSetings {
 
     @Test
     @DisplayName("Позитивный тест кейс")
-    public void  TestRegNdsCase() throws UnirestException {
+    public void  TestRegNdsCase() throws UnirestException, IOException {
 
         String bodyJSON="{\r\n    \"headers\": {\r\n        \"messageUid\": \"" + java.util.UUID.randomUUID() + "\",\r\n        \"messageCreatedDate\": \"2022-01-01 09:34:55\",\r\n        \"operationType\": \"REGISTRATION\"\r\n    },\r\n    \"businessData\": {\r\n        \"taxStatement\": {\r\n            \"registrationType\": 1,\r\n            \"type\": \"1\",\r\n            \"taxOrgCode\": \"6205\",\r\n            \"ndsNzReceiveDate\": \"2022-12-22\",\r\n            \"ndsNzIncomingDate\": \"2022-12-22\",\r\n            \"statementRegReason\": \"REQUIRED\"\r\n        },\r\n        \"taxpayerData\": {\r\n            \"taxpayerCode\": \"430216434014\",\r\n            \"taxpayerType\": \"IP\",\r\n            \"taxpayerName\": {\r\n                \"ru\": \"IP Власова ru\",\r\n                \"kk\": \"IP Власова kk\"\r\n            }\r\n        },\r\n        \"stateAuthorityMark\": {\r\n            \"registrationDate\": \"2020-10-19 19:34:55\",\r\n            \"taxOrgCode\": 6205,\r\n            \"applicationDate\": \"2020-10-19 19:34:55\",\r\n            \"fullName\": \"Иванов П.А.\",\r\n            \"applicantFullName\": \"Кузнецов В.А.\",\r\n            \"applicationNumber\": 10\r\n        }\r\n    }\r\n}";
         JSONObject Response = new JSONObject(Test_api_post.ndsSaveNz(bodyJSON));
@@ -25,6 +27,8 @@ public class IsnaRegNdsIntegrationTest extends BaseSetings {
         Adm adm =new Adm();
 
         adm.ndsJournal().checkCertificate("Выдано свидетельство","430216434014");
+        screenshot1("Скрин последней страницы");
+
         pause(1000);
     }
 
