7a868bd612317923d40872a4579b54974453bdfd
kirill.suntsov <kirillzuzu@gmail.com>
2022-02-25 15:08:58 +0600

    Проверка

--- src/test/java/sapasoft/reg/pages/BasePage.java
+++ src/test/java/sapasoft/reg/pages/BasePage.java
@@ -1,18 +1,20 @@
 package sapasoft.reg.pages;
 
+import com.sun.source.tree.IfTree;
 import io.qameta.allure.Step;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import sapasoft.reg.testconfigs.BaseSetings;
 
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class BasePage extends BaseSetings {
 
-    @Step("Проверерка заголовка")
+    @Step("Проверка заголовка")
     public void checkTitle(String title) {
         $(By.className("antd-pro-components-typography-title-index-title")).text().equals(title);
     }
-
 
 
 
