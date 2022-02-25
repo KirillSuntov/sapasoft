515283c3c2776151aee3931c25660e0bdf5e8e33
SoulOfForge <bektursyn.dastan@gmail.com>
2022-02-25 12:06:03 +0600

    Расширенный Поиск Журнал обработки full 25.02.2022

--- src/test/java/sapasoft/reg/RegSmokeTest.java
+++ src/test/java/sapasoft/reg/RegSmokeTest.java
@@ -21,7 +21,13 @@ public class RegSmokeTest extends BaseSetings {
         reg.processingLogPage().open();
         reg.basePage().checkTitle("Журнал обработки сообщений");
         reg.processingLogPage().advancedSearch();
-        reg.processingLogPage().advancedSearch_NP_Choose("ИП");
+        reg.processingLogPage().advancedSearch_Choose_NP("ИП");
+        reg.processingLogPage().advancedSearch_Choose_Status("Ошибочное");
+        reg.processingLogPage().advancedSearch_Choose_MsgType("Исходящее");
+        reg.processingLogPage().advancedSearch_Choose_MsgSource("ГБД ЕЛ");
+        reg.processingLogPage().advancedSearch_Choose_OperationType("Актуализация");
+        reg.processingLogPage().advancedSearch_Choose_CodeTypeMsg("Ответ со статусом обработки сообщения с уведомлением о начале деятельности в качестве ИП/об изменении сведений ИП");
+
         reg.processingLogPage().advancedSearch_Apply();
     }
 
--- src/test/java/sapasoft/reg/pages/ProcessingLogPage.java
+++ src/test/java/sapasoft/reg/pages/ProcessingLogPage.java
@@ -33,26 +33,62 @@ public class ProcessingLogPage extends BaseSetings {
 //        $(by("data-row-key", "1")).click();
     }
 
-    @Step("Поиск ФЛ в расширенном поиске")
+    @Step("Поиск ИП в расширенном поиске")
     public void advancedSearchFL() {
-        $("[class*=advancedSearch]").click();
         //$(byClassName("ant-select antd-pro-components-select-select-select ant-select-single ant-select-show-arrow")).click();
-        $(byClassName("antd-pro-components-search-by-parameters-index-dropDown")).$(byClassName("ant-select-selection-item")).click();
-        $(byClassName("antd-pro-components-search-by-parameters-index-dropDown")).$(byText("ФЛ")).click();
+        $(byXpath("//*[@id=\"searchByParameters\"]/div[2]/div/div/div/div/form/div[1]/div[1]/div/div[2]/div/div/div/div")).$(byClassName("ant-select-selection-item")).click();
+        $(byClassName("rc-virtual-list-holder-inner")).$(byText("ИП")).click();
         $(byText("Применить")).click();
         $(byClassName("ant-table-tbody")).shouldBe(visible);
         pause(3000);
     }
 
     @Step("Расширенный поиск выбор НП")
-    public void advancedSearch_NP_Choose(String np) {
+    public void advancedSearch_Choose_NP(String np) {
         //$(byClassName("ant-select antd-pro-components-select-select-select ant-select-single ant-select-show-arrow")).click();
-        $(byClassName("antd-pro-components-search-by-parameters-index-dropDown")).$(byClassName("ant-select-selection-item")).click();
-        $(byClassName("antd-pro-components-search-by-parameters-index-dropDown")).$(byText(np)).click();
-        
-        $(byText("Применить")).click();
-        $(byClassName("ant-table-tbody")).shouldBe(visible);
-        pause(3000);
+        $(byXpath("//*[@id=\"searchByParameters\"]/div[2]/div/div/div/div/form/div[1]/div[1]/div/div[2]")).click();
+        $$(byClassName("rc-virtual-list-holder-inner")).get(0).$(byText(np)).click();
+        //pause(3000);
+    }
+
+    @Step("Расширенный поиск выбор Статус сообщения")
+    public void advancedSearch_Choose_Status(String status) {
+        //$(byClassName("ant-select antd-pro-components-select-select-select ant-select-single ant-select-show-arrow")).click();
+        $(byXpath("//*[@id=\"searchByParameters\"]/div[2]/div/div/div/div/form/div[1]/div[2]/div/div[2]/div/div/div/div/div")).click();
+        $$(byClassName("rc-virtual-list-holder-inner")).get(1).$(byText(status)).click();
+        //pause(3000);
+    }
+
+    @Step("Расширенный поиск выбор Вид сообщения")
+    public void advancedSearch_Choose_MsgType(String type) {
+        //$(byClassName("ant-select antd-pro-components-select-select-select ant-select-single ant-select-show-arrow")).click();
+        $(byXpath("//*[@id=\"searchByParameters\"]/div[2]/div/div/div/div/form/div[2]/div[1]/div/div[2]/div/div/div/div")).click();
+        $$(byClassName("rc-virtual-list-holder-inner")).get(2).$(byText(type)).click();
+        //pause(3000);
+    }
+
+    @Step("Расширенный поиск выбор Источник сообщения")
+    public void advancedSearch_Choose_MsgSource(String source) {
+        //$(byClassName("ant-select antd-pro-components-select-select-select ant-select-single ant-select-show-arrow")).click();
+        $(byXpath("/html/body/div[1]/div/section/section/main/div/div[1]/div[2]/div/div[1]/div/div[2]/div/div/div/div/form/div[2]/div[2]/div/div[2]/div/div/div/div")).click();
+        $$(byClassName("rc-virtual-list-holder-inner")).get(3).$(byText(source)).click();
+        //pause(3000);
+    }
+
+    @Step("Расширенный поиск выбор Тип операции")
+    public void advancedSearch_Choose_OperationType(String type) {
+        //$(byClassName("ant-select antd-pro-components-select-select-select ant-select-single ant-select-show-arrow")).click();
+        $(byXpath("/html/body/div[1]/div/section/section/main/div/div[1]/div[2]/div/div[1]/div/div[2]/div/div/div/div/form/div[2]/div[3]/div/div[2]/div/div/div/div")).click();
+        $$(byClassName("rc-virtual-list-holder-inner")).get(4).$(byText(type)).click();
+        //pause(3000);
+    }
+
+    @Step("Расширенный поиск выбор Код типа сообщения")
+    public void advancedSearch_Choose_CodeTypeMsg(String type) {
+        //$(byClassName("ant-select antd-pro-components-select-select-select ant-select-single ant-select-show-arrow")).click();
+        $(byXpath("/html/body/div[1]/div/section/section/main/div/div[1]/div[2]/div/div[1]/div/div[2]/div/div/div/div/form/div[2]/div[4]/div/div[2]/div/div/div/div")).click();
+        $$(byClassName("rc-virtual-list-holder-inner")).get(5).$(byText(type)).click();
+        //pause(3000);
     }
 
     @Step("Расширенный поиск Применить")
