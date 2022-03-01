618fdddd6e09e8d74cd4d8de14bcc609ad5ec6e4
SoulOfForge <bektursyn.dastan@gmail.com>
2022-03-01 11:10:20 +0600

    Расширенный Поиск Журнал обработки 01.03.2022 2

--- src/test/java/sapasoft/reg/pages/ProcessingLogPage.java
+++ src/test/java/sapasoft/reg/pages/ProcessingLogPage.java
@@ -4,8 +4,11 @@ import io.qameta.allure.Step;
 import org.junit.Assert;
 import sapasoft.reg.testconfigs.BaseSetings;
 
+import java.io.IOException;
+
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.screenshot;
 
 public class ProcessingLogPage extends BaseSetings {
 
@@ -94,7 +97,7 @@ public class ProcessingLogPage extends BaseSetings {
     }
 
     @Step("Проверка статуса сообщения с поисковым статусом")
-    public void Check_Equality_Of_Status_To_Search() {
+    public void Check_Equality_Of_Status_To_Search() throws IOException {
        /*System.out.println("CHECK START: ");
         System.out.println("II: " + $(byClassName("ant-table-tbody")).$$(byClassName("ant-table-row")).get(1));
         System.out.println("III: " + $(byClassName("ant-table-tbody")).$(byClassName("ant-table-row")).$$(byClassName("ant-table-cell")));
@@ -108,37 +111,50 @@ public class ProcessingLogPage extends BaseSetings {
 
         }*/
         if(!$$(byClassName("ant-table-tbody")).get(0).$(byClassName("ant-table-row")).$$(byClassName("ant-table-cell")).get(6).getText().equals(this.Msg_Status)){
+            screenshot("Status");
+            screenshot1("Скрин");
             Assert.fail("Значение статуса не соответствует выброному в расширенном поиске");
         }
     }
 
     @Step("Проверка вида сообщения с поисковым видом")
-    public void Check_Equality_Of_MsgType_To_Search() {
+    public void Check_Equality_Of_MsgType_To_Search() throws IOException {
         if(this.Msg_Type == "Исходящее"){
             if(!$$(byClassName("ant-table-tbody")).get(0).$(byClassName("ant-table-row")).$$(byClassName("ant-table-cell")).get(6).getText().equals("Отправлено")){
+                screenshot("Msg_Type1");
+                screenshot1("Скрин");
                 Assert.fail("Значение Вид сообщения не соответствует выброному в расширенном поиске");
             }
         }
         else{
             if($$(byClassName("ant-table-tbody")).get(0).$(byClassName("ant-table-row")).$$(byClassName("ant-table-cell")).get(6).getText().equals("Отправлено")){
+                screenshot("Msg_Type2");
+                screenshot1("Скрин");
                 Assert.fail("Значение Вид сообщения не соответствует выброному в расширенном поиске");
+
             }
         }
     }
 
-    @Step("Проверка источника сообщения с поисковым статусом")
-    public void Check_Equality_Of_MsgSource_To_Search() {
+    @Step("Проверка источника сообщения с поисковым источником")
+    public void Check_Equality_Of_MsgSource_To_Search() throws IOException {
         if(!$$(byClassName("ant-table-tbody")).get(0).$(byClassName("ant-table-row")).$$(byClassName("ant-table-cell")).get(8).getText().equals(this.Msg_Source)){
+            screenshot("Msg_Source");
+            screenshot1("Скрин");
             Assert.fail("Значение Источника сообщения не соответствует выброному в расширенном поиске");
+
         }
     }
 
-    @Step("Проверка Типа операции сообщения с поисковым статусом")
-    public void Check_Equality_Of_OperationType_To_Search() {
+    @Step("Проверка Типа операции сообщения с поисковым типом")
+    public void Check_Equality_Of_OperationType_To_Search() throws IOException {
         System.out.println($$(byClassName("ant-table-tbody")).get(0).$(byClassName("ant-table-row")).$$(byClassName("ant-table-cell")).get(3).getText());
         System.out.println(this.Operation_Type);
         if(!$$(byClassName("ant-table-tbody")).get(0).$(byClassName("ant-table-row")).$$(byClassName("ant-table-cell")).get(3).getText().equals(this.Operation_Type)){
+            screenshot("Operation_Type");
+            screenshot1("Скрин");
             Assert.fail("Значение Тип операции не соответствует выброному в расширенном поиске");
+
         }
     }
 }
